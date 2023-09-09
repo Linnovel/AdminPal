e973f90cdb6d66f908b2083d732519eb8872a937
jose carlos perdomo herrera <phpperdomo@gmail.com>
2023-09-09 08:16:37 -0400

    subiendo endpoint login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +18,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#validamos al usuario y le asignamos un token
+@api.route('/login', methods=['POST'])
+def user_login():
+
+    data = request.get_json()
+    email= data.get('email', None)
+    password= data.get('password', None)
+
+    #validamos que el usrio exista
+    user_exist= User.query.filter_by(email=email).first()
+    if not user_exist:
+        return jsonify({"error":"User not found"}), 404
+    
+    #obtenemos el password y lo comparamos
+    password_check= check_password_hash(user_exist.password, password)
+    if not password_check:
+        return jsonify({"error":"Password incorrecto"}), 401
+   
+    #se crea el token
+    token_data={"id": user_exist.id, "email": user_exist.email}
+    token= create_access_token(token_data)
+    return jsonify({"token": token}), 200
+    
+    
--- src/app.py
+++ src/app.py
@@ -12,6 +12,9 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
+# JWT TOKEN
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -29,6 +32,8 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
+
+jwt = JWTManager(app)
 
 # Allow CORS requests to this API
 CORS(app)
