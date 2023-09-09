e82b0896a751cdcca32b441a9acc1d2d89e4d29b
Luis Linares <luislinnovel@gmail.com>
2023-09-09 10:05:56 -0400

    prueba-primer-commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +19,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('register', methods=['POST'])
+def register_user():
+    
+    data = request.get_json()
+    email = data.get("email", None)
+    password  = data.get("password", None)
+
+    #Encriptacion
+    hashed_password = generate_password_hash(password)
+    new_register = User(email=email, password=hashed_password, is_active=True)
+
+    try:
+        db.session.add(new_register)
+        db.session.commit()
+        return jsonify("Nuevo usuario registrado con exito"), 201
+
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error" : error}), 500
--- src/app.py
+++ src/app.py
@@ -6,11 +6,15 @@ from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+#JWT TOKEN
+from flask_jwt_extended import JWTManager
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+
+
 
 #from models import Person
 
