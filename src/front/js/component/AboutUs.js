5ee4cde42b5d2df6b8e1476a294c0e1b37afed3b
Luis Linares <luislinnovel@gmail.com>
2023-10-22 21:59:52 -0400

    actualizacion de datos de nosotros

--- src/front/js/component/AboutUs.js
+++ src/front/js/component/AboutUs.js
@@ -28,7 +28,7 @@ const AboutUs = () => {
             <div className="col-md-7">
               <div className="card-body p-0">
                 <h2 className="card-title h4 mb-3">Luis Linares</h2>
-                <p className="card-text lead">Diseñador Grafico bilingüe, creatividad e identidad visual. Ahora con estudios y practicas en programación, no solo le doy la identidad a tu marca, ahora puedo crear tu pagina web.</p>
+                <p className="card-text lead">Diseñador Grafico bilingüe, creatividad e identidad visual. Ahora con estudios y practicas en programación, no solo le doy la identidad a tu marca, ahora puedo crear tu pagina web. </p>
                 <p className="card-text lead">Mi desempeño en este proyecto fue mayormente el frontend, usando mi experiencia como diseñador, mi meta fue crear una pagina que le brinde a los usuarios una gran experiencia, una interfaz con todas las caracteristicas utiles para que sea intuitivo y facil de usar.
                 </p>
                 <a
@@ -63,7 +63,7 @@ const AboutUs = () => {
               <div className="card-body p-0">
                 <h2 className="card-title h4 mb-3">Jose Perdomo</h2>
                 <p className="card-text lead">Padre de familia con una amplia experiencia de mas de 12 años de desarrollador PHP, ingenieron en sistemas y siempre con las ganas de ser mejor, ponerme metas y cumplirlas</p>
-                <p className="card-text lead">Mi roll principal fue el backend, usando mi experiencias en ingeniera y PHP logré aplicar las nuevas tecnologías aprendidas mediante el curso para hacer un trabajo optimo y alcanzar la meta y poder entregar este proyecto de manera optima y profesional, con la meta final de llevar esta pagina al mercado actual y poder brindar este servicio al publico deportista.</p>
+                <p className="card-text lead">Mi roll principal fue el backend, usando mi experiencias en ingeniera y PHP logré aplicar las nuevas tecnologías aprendidas mediante el curso para hacer un trabajo optimo y alcanzar la meta para poder entregar este proyecto de manera optima, queremos llevar esta pagina al mercado actual y poder brindar este servicio al publico deportista y emprendedora.</p>
               </div>
             </div>
           </div>
